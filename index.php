57650bb26359706d042fc51f1729e5eb30bd3c80
Darren Williams <design@phoenix1331.co.uk>
2015-05-27 15:31:40 +0100

    Final edits

--- index.php
+++ index.php
@@ -12,24 +12,31 @@
 *
 */
 
-include("inc/functions.php");
+//Set default timezone
+date_default_timezone_set('Europe/London');
 
 // if form is submitted 
 if ($_POST["submit"]) {
 
-$startDate = strtotime($_POST["start-date"]);
-$endDate = strtotime($_POST["end-date"]);
-
-$message = "Difference between two dates is " . abs($startDate - $endDate)/(60*60) . " hour(s)";
-
-// echo $startDate;
-// $startDate = explode("-",$startDate);
-// echo $startDate[1];
+	//Change string to time value
+	$startDateTime = strtotime($_POST["start-date"]);
+	$endDateTime = strtotime($_POST["end-date"]);
+	//Format date for returned message
+	$startDate = date("D d M Y",$startDateTime);
+	$endDate = date("D d M Y",$endDateTime);
+	//Calculate difference between dates
+	$calcDays = abs($startDateTime - $endDateTime)/(60*60*24);
+	$calcHours = abs($startDateTime - $endDateTime)/(60*60);
+	$calcMinutes = abs($startDateTime - $endDateTime)/60;
+	$calcSeconds = abs($startDateTime - $endDateTime);
+	//Compile return message
+	$message = "Total time between " . $startDate . " and " . $endDate ." is: <br /><br />";
+	$message .= $calcDays . " Day(s)<br />" . $calcHours . " Hour(s)<br />";
+	$message .= $calcMinutes . " Minute(s)<br />" . $calcSeconds . " Second(s)<br />";
 
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html>
@@ -43,22 +50,23 @@ $message = "Difference between two dates is " . abs($startDate - $endDate)/(60*6
 	<link href="css/style.css" rel="stylesheet">
 </head>
 <body>
+<p class="intro">Please pick a start date and an end date.</p>
 <div class="form-wrap">
-	<p>Please pick a start date and an end date.</p>
 	<form class="date-form" role="form" method="post" action="">
 		<label for="start-date">Start Date:</label><br />
-		<input type="date" name="start-date" value="2015-05-27"><br />
+		<input type="date" name="start-date" value="<?php echo date("Y-m-d"); ?>"><br />
 		<label for="end-date">End Date:</label><br />
-		<input type="date" name="end-date" value="2015-05-28"><br />
-		<input id="submit" name="submit" type="submit" value="submit">
+		<input type="date" name="end-date" value="<?php if(isset($endDateTime)){echo date("Y-m-d",$endDateTime);} ?>"><br />
+		<input class="submit" name="submit" type="submit" value="submit">
 	</form>
-	<div class="message">
+</div>
+<div class="message">
 	<?php
+		//output message if set
 		if(isset($message)){
 			echo $message;
 		}
 	?>
-	</div>
 </div>
 </body>
 </html>
